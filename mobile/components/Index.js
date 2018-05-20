0d756ea2d92eb4270d4b7354e04fdb1ed4b2e657
andreGarvin <andregarvin718@gmail.com>
2018-05-19 22:11:39 -0400

    stack navigation

--- mobile/components/Index.js
+++ mobile/components/Index.js
@@ -1,10 +1,35 @@
-import React, { Component } from 'react'
-import { Text } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+import Messaging from './Messages';
+
+class Home extends Component {
+  render() {
+    return (
+      <View>
+        <Text>HOME</Text>
+        <Button
+          title="Go to messages"
+          onPress={() =>
+            this.props.navigation.navigate('Messaging')
+          }
+        />
+      </View>
+    )
+  }
+}
+
+const Root = createStackNavigator({
+  Home: Home,
+  Messaging,
+  // Sessions, 
+})
 
 class App extends Component {
   render() {
     return (
-      <Text>INDEX</Text>
+      <Root />
     )
   }
 }
